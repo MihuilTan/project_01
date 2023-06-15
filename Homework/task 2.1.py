623c8b33907011bea7fbfce0d088e7312bc70b5e
Михаил <134321691+MihuilTan@users.noreply.github.com>
2023-06-15 16:23:12 +0300

    Add files via upload

--- Homework/task 2.1.py
+++ Homework/task 2.1.py
@@ -11,7 +11,116 @@
 # функции sorted, max и min использовать нельзя!
 
 
-arr = [12, 4, 31, -7, 6, 33, 19]
+arr = [4,6,2,1,9,63,-134,566]
+print (arr)
+
+def minimum(arr):
+    i=0
+    length = len(arr)
+    while i < length - 1:
+        m = i
+        j = i + 1
+        while j < length:
+            if arr[j]< arr[m]:
+                m = j
+            j +=1
+        arr[i], arr[m] = arr[m], arr[i]
+        i += 1
+    return arr
+print (' -> min', minimum(arr)[0])
+
+def maximum(arr):
+    i=0
+    length = len(arr)
+    while i < length - 1:
+        m = i
+        j = i + 1
+        while j < length:
+            if arr[j]< arr[m]:
+                m = j
+            j +=1
+        arr[i], arr[m] = arr[m], arr[i]
+        i += 1
+    return arr
+print (' -> max', maximum(arr)[-1])
+
+
+
+
+arr = [-52, 56, 30, 29, -54, 0, -110]
+print (arr)
+
+def minimum(arr):
+    i=0
+    length = len(arr)
+    while i < length - 1:
+        m = i
+        j = i + 1
+        while j < length:
+            if arr[j]< arr[m]:
+                m = j
+            j +=1
+        arr[i], arr[m] = arr[m], arr[i]
+        i += 1
+    return arr
+print (' -> min', minimum(arr)[0])
+
+def maximum(arr):
+    i=0
+    length = len(arr)
+    while i < length - 1:
+        m = i
+        j = i + 1
+        while j < length:
+            if arr[j]< arr[m]:
+                m = j
+            j +=1
+        arr[i], arr[m] = arr[m], arr[i]
+        i += 1
+    return arr
+print (' -> max', maximum(arr)[-1])
+
+
+
+
+arr = [42, 54, 65, 87, 0] 
+print (arr)
+
+def minimum(arr):
+    i=0
+    length = len(arr)
+    while i < length - 1:
+        m = i
+        j = i + 1
+        while j < length:
+            if arr[j]< arr[m]:
+                m = j
+            j +=1
+        arr[i], arr[m] = arr[m], arr[i]
+        i += 1
+    return arr
+print (' -> min', minimum(arr)[0])
+
+def maximum(arr):
+    i=0
+    length = len(arr)
+    while i < length - 1:
+        m = i
+        j = i + 1
+        while j < length:
+            if arr[j]< arr[m]:
+                m = j
+            j +=1
+        arr[i], arr[m] = arr[m], arr[i]
+        i += 1
+    return arr
+print (' -> max', maximum(arr)[-1])
+
+
+
+
+
+arr = [5]   
 print (arr)
 
 def minimum(arr):
